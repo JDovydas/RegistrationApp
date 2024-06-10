9e86aa5eea35a11e139987d5209013afe00c5605
JDovydas <149430035+JDovydas@users.noreply.github.com>
2024-06-10 18:04:54 +0300

    06 10 last?

--- ControllerTest/PersonInformationControllerTests.cs
+++ ControllerTest/PersonInformationControllerTests.cs
@@ -47,73 +47,6 @@ namespace ControllerTest
         }
 
         [Fact]
-        public async Task AddPersonInformation_ShouldReturnOkResult_WhenPersonInfoIsAddedSuccessfully()
-        {
-            // Arrange
-            var personDto = new PersonDto
-            {
-                Name = "Jonas",
-                LastName = "Pirmasis",
-                BirthDate = "1990-01-01"
-            };
-
-            var placeOfResidenceDto = new PlaceOfResidenceDto
-            {
-                City = "Vilnius",
-                Street = "Gedimino pr.",
-                HouseNumber = 1
-            };
-
-            // Birth date will to be used in mock setup.
-            DateOnly birthDate = DateOnly.FromDateTime(DateTime.Parse("1990-01-01"));
-
-            // returns true when called with any string as argument and to output birthDate variable.
-            _personServiceMock.Setup(s => s.ValitateBirthDate(It.IsAny<string>(), out birthDate))
-                              // Callback method allows setting out parameter when ValitateBirthDate method is called.
-                              .Callback((string input, out DateOnly date) =>
-                              {
-                                  // Out parameter is set to specified birth date.
-                                  date = DateOnly.FromDateTime(DateTime.Parse("1990-01-01"));
-                              })
-                              .Returns(true);
-
-            // mock - return completed Task when called with any Guid, specified personDto, placeOfResidenceDto, null and birthDate.
-            _personServiceMock.Setup(s => s.AddPersonInformationAsync(It.IsAny<Guid>(), personDto, placeOfResidenceDto, null, birthDate))
-                               // Returns method specifies return value of mocked method.
-                               .Returns(Task.CompletedTask);
-
-            // Act
-            var result = await _controller.AddPersonInformation(personDto, placeOfResidenceDto);
-
-            // Assert
-            var okResult = Assert.IsType<OkObjectResult>(result);
-            Assert.Equal("Person information added successfully.", okResult.Value);
-        }
-
-        [Fact]
-        public async Task UpdateName_ShouldReturnOkResult_WhenNameIsUpdatedSuccessfully()
-        {
-            // Arrange
-            var personId = Guid.NewGuid();
-            var newName = "Petras";
-
-            // Setup mock to simulate a successful update
-            // UpdateNameAsync is called with any userId, specified personId, and newName,
-            // Return completed Task, indicating that update was successful.
-            _personServiceMock.Setup(s => s.UpdateNameAsync(It.IsAny<Guid>(), personId, newName))
-                              .Returns(Task.CompletedTask);
-
-            // Act
-            var result = await _controller.UpdateName(personId, newName);
-
-            // Assert
-            // Check if result is of type OkObjectResult, indicating successful operation.
-            var okResult = Assert.IsType<OkObjectResult>(result);
-            // Verify that message in OkObjectResult is "Name updated successfully."
-            Assert.Equal("Name updated successfully.", okResult.Value);
-        }
-
-        [Fact]
         public async Task UpdateGender_ShouldReturnOkResult_WhenGenderIsUpdatedSuccessfully()
         {
             // Arrange
@@ -122,7 +55,7 @@ namespace ControllerTest
 
             // Setup mock to simulate successful update
             // UpdateGenderAsync is called with any userId, specified personId, and newGender,
-            // Return completed Task, indicating that update was successful.
+            // Return completed Task
             _personServiceMock.Setup(s => s.UpdateGenderAsync(It.IsAny<Guid>(), personId, newGender))
                               .Returns(Task.CompletedTask);
 
@@ -130,7 +63,7 @@ namespace ControllerTest
             var result = await _controller.UpdateGender(personId, newGender);
 
             // Assert
-            // Check if result is of type OkObjectResult, indicating successful operation.
+            // Check if result is of type OkObjectResult indicating successful operation.
             var okResult = Assert.IsType<OkObjectResult>(result);
 
             // Verify that message in OkObjectResult is "Gender updated successfully."
